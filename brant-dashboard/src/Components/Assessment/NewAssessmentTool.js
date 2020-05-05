f63eff72f79c5e35d3c0bdeda8550739579cc53a
lmiguelg <lmiguelgoncalves97@hotmail.com>
2020-05-05 20:43:54 +0100

    update newAssessmentTool.js (working on submit the form)

--- brant-dashboard/src/Components/Assessment/NewAssessmentTool.js
+++ brant-dashboard/src/Components/Assessment/NewAssessmentTool.js
@@ -7,33 +7,73 @@ function NewAssessmentTool(props){
 
     const [domains,setDomains] = useState([]);
 
+    //input left side
     const [newDomain,setNewDomain] = useState("");
 
-    //id do dominio que será editado para criar os subdominios
-    const [domainEditing,setdomainEditing] = useState(0);
-    
-    const onFormSubmit = (e) => {
-        e.preventDefault();
-           
-    };
+    //input right side
+    const [newSubDomain,setNewSubDomain] = useState("");
 
-    let domain = {id:domains.length,name:newDomain,val:0}
- 
+    let domain = {id:0, name:newDomain,val:0,subDomains:[]}
+    
+    if(domains !== undefined && domains.length > 0 )
+        domain = {id:domains[domains.length - 1].id+1,name:newDomain,val:0,subDomains:[]}
+  
+    //id do dominio que será editado para criar os subdominios
+    const [domainEditing,setdomainEditing] = useState("");
+
+    const [subDomainEditing,setSubDomainEditing] = useState("");
+    
+   
+
+    let subDomain = {id:0, name:newSubDomain,val:0}
+
+    if(domainEditing.subDomains !== undefined && domainEditing.subDomains.length > 0)
+        subDomain = {id:domainEditing.subDomains[domainEditing.subDomains.length - 1].id+1,name:newSubDomain,val:0}
+    
     const onChangeNewDomain = (e)=>{
         setNewDomain(e.target.value);
-        
+    }
+    const onChangeNewSubDomain = (e)=>{
+        setNewSubDomain(e.target.value);
     }
 
-    
-
     const onclickAddDomain=() => {
-        // Should not ever set state during rendering, so do this in useEffect instead.  
+        
         if(domain.name !== "")
             setDomains([...domains, domain]);
         setNewDomain("");
 
     }
+    const onclickAddSubDomain=() => {
+        let tempDomain = domainEditing;
+        if(subDomain.name !== "" &&tempDomain.subDomains !== undefined)
+            tempDomain.subDomains.push(subDomain);
 
+        setdomainEditing(tempDomain);      
+        setNewSubDomain("");
+
+    }
+
+    const onclickDeleteDomain=() => {
+    
+        setDomains(domains.filter(item => item.id != domainEditing.id));
+        setdomainEditing("");    
+    }
+    const onclickDeleteSubDomain=() => {
+        let tempDomain = domainEditing;
+        tempDomain.subDomains = domainEditing.subDomains.filter(item => item.id != subDomainEditing.id);
+        setdomainEditing(tempDomain);  
+        
+        setSubDomainEditing("");    
+    }
+
+
+    const onFormSubmit = (e) => {
+        e.preventDefault(); 
+        /******************************************** */
+        //juntar a variavel do nome, decrição e domains
+        //e submeter aqui
+    };
     return <div className="container-fluid mt-2">
         <form onSubmit={onFormSubmit}>
             <Title sectionTitle="Criar novo teste de avaliação"/>
@@ -47,7 +87,7 @@ function NewAssessmentTool(props){
                     <div className="col-sm-6">
                         <div className="pl-4 pr-4 pt-2">
                             <label className="text-brant-color">Descrição</label>
-                            <textarea type="name" className="form-control" id="inputDescription" placeholder="Inserir nove"/>
+                            <textarea type="name" className="form-control" id="inputDescription" placeholder="Inserir descrição"/>
                         </div>
                     </div>    
                 </div>
@@ -56,12 +96,14 @@ function NewAssessmentTool(props){
                         <div className="card">
                             <div className="card-header">
                                 <Subtitle sectionTitle="Domínios"/>
+                                
                                 <input type="name" className="form-control w-80" id="inputName" value={newDomain} onChange={onChangeNewDomain} placeholder="Inserir novo dominio"/>
                                 <button onClick={onclickAddDomain} className="btn btn-brant-color mt-1">Adicionar</button>
+                                <button className="btn btn-danger ml-4" onClick={onclickDeleteDomain}>Eliminar</button>
                             </div>
                             <div className="card-body p-0">
                                 <ul className="list-group list-group-flush">
-                                    {domains !== undefined ? domains.map(temp => <li key={temp.id} onClick={() => {setdomainEditing(temp)}} className="list-group-item list-group-item-action">{temp.name}<input type="checkbox"/></li> ):null}
+                                    {domains !== undefined && domainEditing.name !== "" ? domains.map(temp => <li key={temp.id} onClick={() => {setdomainEditing(temp)}} className={temp.id === domainEditing.id?"list-group-item list-group-item-brant-color list-group-item-action active":"list-group-item list-group-item-brant-color list-group-item-action"}>{temp.name}</li> ):null}
                                 </ul>
                             </div>
                         </div>
@@ -70,11 +112,15 @@ function NewAssessmentTool(props){
                     <div className="col-sm-6">
                         <div className="card">
                             <div className="card-header">
-                                {domainEditing.name !== undefined ?<Subtitle sectionTitle={"Subdomínios: "+domainEditing.name}/> : <Subtitle sectionTitle="Subdomínios - escolher domínio a editar"/>}
-                                <button className="btn btn-brant-color">Adicionar subdomínio</button>
+                                {domainEditing.name !== undefined && domainEditing.name !== "" ? <Subtitle sectionTitle={"Subdomínios: "+domainEditing.name}/> : <Subtitle sectionTitle="Subdomínios - escolher domínio a editar"/>}
+                                <input type="name" className="form-control w-80" id="inputSubdomain" value={newSubDomain} onChange={onChangeNewSubDomain} placeholder="Inserir novo dominio"/>
+                                <button onClick={onclickAddSubDomain} className="btn btn-brant-color mt-1">Adicionar</button>
+                                <button className="btn btn-danger ml-4" onClick={onclickDeleteSubDomain}>Eliminar</button>
                             </div>
                             <div className="card-body">
-                                sdsdsds
+                                <ul className="list-group list-group-flush">
+                                    {domainEditing.subDomains !== undefined && domainEditing.subDomains.length > 0 ? domainEditing.subDomains.map(temp => <li key={temp.id} onClick={() => {setSubDomainEditing(temp)}} className={temp.id === subDomainEditing.id?"list-group-item list-group-item-brant-color list-group-item-action active":"list-group-item list-group-item-brant-color list-group-item-action"}>{temp.name}</li> ):null}
+                                </ul>
                             </div>
                         </div>
                         
