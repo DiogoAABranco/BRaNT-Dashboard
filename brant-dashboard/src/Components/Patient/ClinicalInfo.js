f0c4e5635dd48095f003911bcddd52cbc0557035
lmiguelg <lmiguelgoncalves97@hotmail.com>
2020-06-22 02:25:03 +0100

    patient information complete

--- brant-dashboard/src/Components/Patient/ClinicalInfo.js
+++ brant-dashboard/src/Components/Patient/ClinicalInfo.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
 import MaterialTable from 'material-table'
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Title from '../Others/Title'
 
 
-function ClinicalInfo({clinicalTypes,clinicalInfo}){
+function ClinicalInfo({clinicalTypes,clinicalInfo,handleSubmit, description, setDescription, clinical_info_type_id, setClinical_info_type_id}){
   
     function setClinicalType (id){
        let typeC = "";
@@ -15,12 +20,42 @@ function ClinicalInfo({clinicalTypes,clinicalInfo}){
         return typeC;
     }
 
-    console.log();
     return <div>
+        <div className="col-md-12 d-flex justify-content-center">
+            <Title sectionTitle="Criar nova informação"/>
+        </div>
+        <form action="" onSubmit={handleSubmit}>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-6">
+                        <textarea className="form-control form-control-sm border-brant-color m-4 w-100" rows="4" value={description} onChange={event => setDescription(event.target.value)} placeholder="Inserir descrição" required></textarea>
+                    </div>
+                    <div className="col-md-6 d-flex justify-content-center align-self-center">
+                    <FormControl className="w-50">
+                        <InputLabel id="demo-controlled-open-select-label">Selecionar Tipo</InputLabel>
+                        <Select
+                            labelId="demo-customized-select-label"
+                            id="demo-customized-select"
+                            value={clinical_info_type_id}
+                            onChange={event => setClinical_info_type_id(event.target.value)}>
+
+                            {clinicalTypes.map(temp =>
+                                <MenuItem key={temp.id} value={temp.id}>{temp.type}</MenuItem>
+                            )}
+                        </Select>
+                    </FormControl>
+                    </div>
+                    <div className="col-md-12 d-flex justify-content-center mb-2">
+                        <button type="submite" className="btn btn-brant-color mt-2 w-40">Adicionar</button>
+                    </div>
+                </div>
+            </div>
+            
+        </form>
             <MaterialTable
             options={{
                 search: true,
-                paging: false,
+                paging: true,
                 showTitle:false,
                 header:true,
                 headerStyle:{
@@ -30,11 +65,9 @@ function ClinicalInfo({clinicalTypes,clinicalInfo}){
                 }
             }}
             columns={[
-                { title: 'Tipo', field: 'clinical_info_type_id', render: rowData => {if(rowData.clinical_info_type_id != null)return setClinicalType(rowData.clinical_info_type_id)},
-        
-                },
+                { title: 'Tipo', field: 'clinical_info_type_id', render: rowData => {if(rowData.clinical_info_type_id != null)return setClinicalType(rowData.clinical_info_type_id)},},
                 { title: 'Descrição', field: 'description'},
-                { title: 'Data', field: 'date'}
+                // { title: 'Data', field: 'date'}
         ]}
         data={clinicalInfo}
         />
--- brant-dashboard/src/Components/Patient/PatientInformation.js
+++ brant-dashboard/src/Components/Patient/PatientInformation.js
@@ -11,6 +11,12 @@ export default function PatientInformation(props) {
     const [patient, setPatient] = useState(null);
     const [clinicalTypes, setClinicalTypes] = useState();
 
+    const [description, setDescription] = useState("");
+    const [clinical_info_type_id, setClinical_info_type_id] = useState(0);
+
+    const [success, setSuccess] = useState(false);
+    const [warning, setWarning] = useState(false);
+
     const apiCallFields = () => {
         const abortController = new AbortController();
         const signal = abortController.signal;
@@ -60,12 +66,50 @@ export default function PatientInformation(props) {
 
     },[]);
 
-    
-  console.log(patient);
-  console.log(clinicalTypes);
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        let data={description,clinical_info_type_id};
+        console.log(JSON.stringify(data));
+
+        fetch('http://localhost:8000/api/clinical-info/1', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => {
+            
+            if(res.status==201){
+                setSuccess(true);
+                setWarning(false);
+                apiCallPatient();
+                
+            }else{
+                setSuccess(false);
+                setWarning(true);
+             
+            }
+            return res;
+        })
+        .then(res => res.json())
+        .then((data) => {
+            console.log('API success: ',data);
+        })
+        .catch(err => {
+            setSuccess(false);
+            setWarning(true);
+            return err;
+        });
+        setDescription("");
+        setClinical_info_type_id(0);
+     
+    }
   
     return (
         <div className="container w-100">
+            {success?<div className="alert alert-success" role="alert">Informação adicionada!</div>:null }
+            {warning?<div className="alert alert-danger" role="alert">Não foi possível adicionar informação</div>:null }
         <div className="row">
             <div className="col-md-12">
                 <nav>
@@ -85,7 +129,7 @@ export default function PatientInformation(props) {
                         
                         <h1>
                         {patient !== null && clinicalTypes !== null?
-                        (console.log(clinicalTypes),<ClinicalInfo clinicalTypes={clinicalTypes} clinicalInfo={patient.clinical_info}/>):null}
+                        <ClinicalInfo clinicalTypes={clinicalTypes} clinicalInfo={patient.clinical_info} handleSubmit={handleSubmit} description={description} setDescription={setDescription} clinical_info_type_id={clinical_info_type_id} setClinical_info_type_id={setClinical_info_type_id}/>:null}
                            
                         </h1>
                     </div>
@@ -96,6 +140,7 @@ export default function PatientInformation(props) {
                     </div>
                 </div>
             </div>
+            
         </div>
     </div>
     );
